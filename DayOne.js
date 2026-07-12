bf78af0708f69c2fba3b3dad3ddb6571e2bde4c3
salma-fathy65 <salmafathy1311@gmail.com>
2026-07-12 21:15:26 +0300

    add logic

---
+++ DayOne.js
@@ -0,0 +1,19 @@
+//1st Task
+var x = Number(window.prompt("Enter first number"));
+var y = Number(window.prompt("Enter second number"));
+
+//sum
+console.log(x+y);
+//subtraction
+console.log(x-y);
+//multiplication
+console.log(x*y);
+//Division
+console.log(x/y);
+
+//2nd Task
+var firstName = window.prompt("Enter first name");
+var lastName = window.prompt("Enter last name");
+var age = window.prompt("Enter your age");
+
+console.log("Hello " + firstName + " " + lastName + ", your age is " + age )
